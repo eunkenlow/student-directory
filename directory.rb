d00a51f1f88fe4422c8e69658bc67b07cb9ec54e
K <K>
2016-09-15 00:13:57 +0800

    added specific names only

--- directory.rb
+++ directory.rb
@@ -5,8 +5,8 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+     students.each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -23,11 +23,17 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    name.downcase!
+    if name.include?('h')
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    else
+    puts "Enter a name that starts with h"
+    name = gets.chomp
+    end
   end
     # return the array of students
     students
